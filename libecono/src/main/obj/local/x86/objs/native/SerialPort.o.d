5cb634e72830b455e1e37ba2ea08f03a430ffaf7
Sawan Roy <sawan.roy25@gmail.com>
2023-09-25 10:44:50 +0530

    Initial Commit

---
+++ libecono/src/main/obj/local/x86/objs/native/SerialPort.o.d
@@ -0,0 +1,5 @@
+C:/Users/Pc/OneDrive/Desktop/libecono/libecono/src/main/obj/local/x86/objs/native/SerialPort.o: \
+  C:\Users\Pc\OneDrive\Desktop\libecono\libecono\src\main\jni\SerialPort.c \
+  C:\Users\Pc\OneDrive\Desktop\libecono\libecono\src\main\jni\com_flownex_libecono_Rs232.h
+
+C:\Users\Pc\OneDrive\Desktop\libecono\libecono\src\main\jni\com_flownex_libecono_Rs232.h:
